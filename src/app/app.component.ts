dae078fed1f22bbf3d0a13cdf323d8eb075e97a2
ebgarcia <166623651+saca1983@users.noreply.github.com>
2025-02-01 18:40:55 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { SearchComponent } from './pages/search/search.component';
+import { MostComponent } from './pages/most/most.component';
+import { LessComponent } from './pages/less/less.component';
+import { ReservedComponent } from './pages/reserved/reserved.component';
+import { QualifiedComponent } from './pages/qualified/qualified.component';
+import { NewComponent } from './pages/new/new.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomeComponent, SearchComponent],
+  imports: [RouterOutlet, WelcomeComponent, SearchComponent, MostComponent, LessComponent, ReservedComponent, QualifiedComponent, NewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { SearchComponent } from './pages/search/search.component';
+import { MostComponent } from './pages/most/most.component';
+import { LessComponent } from './pages/less/less.component';
+import { ReservedComponent } from './pages/reserved/reserved.component';
+import { QualifiedComponent } from './pages/qualified/qualified.component';
+import { NewComponent } from './pages/new/new.component';
 
 export const routes: Routes = [
     {path: 'welcome', component: WelcomeComponent},
     {path: 'search', component: SearchComponent},
+    {path: 'most', component: MostComponent},
+    {path: 'less', component: LessComponent},
+    {path: 'reserved', component: ReservedComponent},
+    {path: 'qualidied', component: QualifiedComponent},
+    {path: 'new', component: NewComponent},
 ];
